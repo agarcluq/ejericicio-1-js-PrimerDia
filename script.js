d6ea035c381ca45902100974f8f0c5bf7ae376b3
agarcluq <agarcluq@everis.com>
2020-11-10 12:33:34 +0100

    ejercicio 1 acabado

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // En este ejercicio deberéis crear un array de objetos a partir de la siguiente lista de usuarios:
-//  * name: usuario1, country: spain, money: 199, premiumAccount: true 
-//  * name: usuario2, country: france, money: 0, premiumAccount: false 
-//  * name: usuario3, country: spain, money: 537, premiumAccount: false 
-//  * name: usuario4, country: italy, money: 1004, premiumAccount: true 
-//  * name: usuario5, country: spain, money: 250, premiumAccount: false 
-//  * name: usuario6, country: ireland, money: 799, premiumAccount: true 
-//  * name: usuario7, country: spain, money: 3345, premiumAccount: false 
+//  * name: usuario1, country: spain, money: 199, premiumAccount: true
+//  * name: usuario2, country: france, money: 0, premiumAccount: false
+//  * name: usuario3, country: spain, money: 537, premiumAccount: false
+//  * name: usuario4, country: italy, money: 1004, premiumAccount: true
+//  * name: usuario5, country: spain, money: 250, premiumAccount: false
+//  * name: usuario6, country: ireland, money: 799, premiumAccount: true
+//  * name: usuario7, country: spain, money: 3345, premiumAccount: false
 
 // Este array de usuarios debe generarse en una función llamada "createUsers". Al generar el array hay que asignarlo a una variable que haya sido declarada globalmente fuera de la función.
 
@@ -15,9 +15,97 @@
 // Las dos últimas instrucciones de la función "onLoad" deben ser dos console.log para mostrar los dos arrays.
 // Utilizad constantes siempre que sea posible.
 // Suerte!
+console.log("hola");
+//  * name: usuario1, country: spain, money: 199, premiumAccount: true
+//  * name: usuario2, country: france, money: 0, premiumAccount: false
+//  * name: usuario3, country: spain, money: 537, premiumAccount: false
+//  * name: usuario4, country: italy, money: 1004, premiumAccount: true
+//  * name: usuario5, country: spain, money: 250, premiumAccount: false
+//  * name: usuario6, country: ireland, money: 799, premiumAccount: true
+//  * name: usuario7, country: spain, money: 3345, premiumAccount: false
 
-window.addEventListener('load',onLoad);
+/**CREANDO LA VARIABLE USERS LA ORIGINAL */
+var users;
+/**CREANDO FUNCIÓN USERS LA ORIGINAL: CREA Y MUESTRA*/
+function createUsers() {
+  var persona1 = {
+    name: "usuario1",
+    country: " spain",
+    money: 199,
+    premiumAccount: true
+  };
 
-function onLoad(){
-  console.log('hi')
+  var persona2 = {
+    name: "usuario2",
+    country: " france",
+    money: 0,
+    premiumAccount: false
+  };
+  var persona3 = {
+    name: " usuario3",
+    country: " spain",
+    money: 537,
+    premiumAccount: false
+  };
+  var persona4 = {
+    name: "usuario4",
+    country: "italy",
+    money: 1004,
+    premiumAccount: true
+  };
+  var persona5 = {
+    name: "usuario5",
+    country: "spain",
+    money: 250,
+    premiumAccount: false
+  };
+  var persona6 = {
+    name: "usuario6",
+    country: "ireland",
+    money: 799,
+    premiumAccount: true
+  };
+  var persona7 = {
+    name: "usuario7",
+    country: "spain",
+    money: 3345,
+    premiumAccount: false
+  };
+  users = [
+    persona1,
+    persona2,
+    persona3,
+    persona4,
+    persona5,
+    persona6,
+    persona7
+  ];
+  for (i = 0; i < users.length; i++) {
+    console.log(users[i]);
+    console.log("okei");
+  }
+}
+
+/**CREANDO LA VARIABLE US2 y us1 para guardar los dos arrays */
+let us2;
+let us1;
+/**CREANDO FUNCIÓN USERS FILTRADA */
+function filterUsers() {
+  for (i = 0; i < users.length; i++) {
+    if (users[i].country == "spain" && users[i].money > 200) {
+      console.log(users[i]);
+      console.log("ok");
+    }
+  }
+}
+
+window.addEventListener("load", onLoad);
+/**MUESTRA AMBOS ARRAYS*/
+function onLoad() {
+  console.log("================ARRAY SIN FILTRAR================");
+  us1 = createUsers();
+  console.log(us1);
+  console.log("================ARRAY FILTRADO================");
+  us2 = filterUsers();
+  console.log(us2);
 }
